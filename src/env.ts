777a2aa145c6f955057c73ef608f3c40a53eb766
adii1203 <aditya32ft@gmail.com>
2025-09-30 15:05:48 +0530

    feat: Add type safe env variables

---
+++ src/env.ts
@@ -0,0 +1,32 @@
+/* eslint-disable node/no-process-env */
+import { config } from "dotenv";
+import { expand } from "dotenv-expand";
+import path from "node:path";
+import { z } from "zod";
+
+expand(config({
+  path: path.resolve(
+    process.cwd(),
+    process.env.NODE_ENV === "test" ? ".env.test" : ".env",
+  ),
+}));
+
+const EvnSchema = z.object({
+  NODE_ENV: z.string().default("development"),
+  PORT: z.coerce.number().default(9999),
+  LOG_LEVEL: z.enum(["fatal", "error", "warn", "info", "debug", "trace"]),
+});
+
+type env = z.infer<typeof EvnSchema>;
+
+// eslint-disable-next-line ts/no-redeclare
+const { data: env, error } = EvnSchema.safeParse(process.env);
+
+if (error) {
+  console.warn("Invalid env:");
+
+  console.warn(z.treeifyError(error).properties);
+
+  process.exit(1);
+}
+export default env!;
